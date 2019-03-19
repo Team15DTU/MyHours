f514cc94c00735dc0caaaac2358b3ddf2b6e56d4
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-03-19 16:01:24 +0100

    Added all DAO's and Interfaces for DAO's:
    IShiftDAO
    ShiftDAO
    
    IJobDAO
    JobDAO
    
    IWorkerDAO
    WorkerDAO
    
    IWorkPlaceDAO
    WorkPlaceDAO

---
+++ src/main/java/DAO/shift/IShiftDAO.java
@@ -0,0 +1,33 @@
+package DAO.shift;
+
+import DTOs.shift.ShiftDTO;
+import DTOs.worker.WorkerDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Rasmus Sander Larsen
+ */
+public interface IShiftDAO {
+
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+
+    // returns a ShiftDTO from an UserID and a Date and time of the shift TODO: Maybe use ShiftID?.
+    ShiftDTO getShift (int userID, LocalDateTime dateAndTime);
+
+    // Returns a List of ShiftDTO object.
+    List<ShiftDTO> getShiftList (WorkerDTO worker);
+
+    // Inserts the data from a ShiftDTO into DB.
+    void createShift (ShiftDTO shift);
+
+    // Updates the data on a Shifts row in the DB.
+    void updateShift (ShiftDTO shift);
+
+    // Deletes all information about one Shift, from an UserID and a Date and time of the shift TODO: Maybe use ShiftID?.
+    void deleteShift (int userID, LocalDateTime dateAndTime);
+
+}
