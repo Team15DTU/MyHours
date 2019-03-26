cb4a9e6cf6ea2605602f0c0fce319fcb12a26494
Logstor <a.rottger_rydahl@live.dk>
2019-03-26 17:32:37 +0100

    Created WorkerDAOTest.java
    Added JUnit dependency to pom.xml

---
+++ src/test/java/DAO/WorkerDAOTest.java
@@ -0,0 +1,33 @@
+package DAO;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WorkerDAOTest {
+	
+	@Test
+	public void getWorker()
+	{
+	}
+	
+	@Test
+	public void getWorkerList()
+	{
+	}
+	
+	@Test
+	public void createWorker()
+	{
+	}
+	
+	@Test
+	public void updateWorker()
+	{
+	}
+	
+	@Test
+	public void deleteWorker()
+	{
+	}
+}
