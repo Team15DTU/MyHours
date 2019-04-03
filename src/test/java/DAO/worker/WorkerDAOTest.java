c76209b12467c7d1958f5c41873fb610a1c65ce6
Logstor <a.rottger_rydahl@live.dk>
2019-04-03 20:55:42 +0200

    WorkerDAOTest.java  - 1st iteration of getWorkerTest()  - Made the tests run in order

--- src/test/java/DAO/worker/WorkerDAOTest.java
+++ src/test/java/DAO/worker/WorkerDAOTest.java
@@ -4,14 +4,21 @@ import DAO.Conn;
 import DAO.DALException;
 import DTOs.address.Address;
 import DTOs.address.IAddress;
+import DTOs.workPlace.IWorkPlaceDTO;
 import DTOs.worker.IWorkerDTO;
 import DTOs.worker.WorkerDTO;
 import db.DBController;
 import db.IConnPool;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 
+
+@FixMethodOrder(MethodSorters.JVM)		// Make sure tests run in order
 public class WorkerDAOTest {
 	
 	//region Test Material
@@ -48,9 +55,31 @@ public class WorkerDAOTest {
 	}
 	
 	@Test
-	public void getWorker()
+	public void getWorker() throws DALException
 	{
-	
+		IWorkerDAO workerDAO = new DBController(connPool).getiWorkerDAO();
+		
+		// Try to Create them
+		for (IWorkerDTO worker : testWorkers)
+			workerDAO.createWorker(worker, "FuckingPassword");
+		
+		// Validate DB data
+		for (IWorkerDTO worker : testWorkers)
+		{
+			IWorkerDTO fromDB = workerDAO.getWorker(worker.getEmail());
+			
+			assertEquals(fromDB.getFirstName(), worker.getFirstName());
+			assertEquals(fromDB.getSurName(), worker.getSurName());
+			assertEquals(fromDB.getEmail(), worker.getEmail());
+			assertEquals(fromDB.getBirthday(), worker.getBirthday());
+			assertEquals(fromDB.getHomeAddress(), worker.getHomeAddress());
+			for (IWorkPlaceDTO workPlace : worker.getWorkPlaces())
+				assertTrue(fromDB.getWorkPlaces().contains(workPlace));
+		}
+		
+		// Delete again
+		for (IWorkerDTO worker : testWorkers)
+			workerDAO.deleteWorker(worker.getEmail());
 	}
 	
 	@Test
