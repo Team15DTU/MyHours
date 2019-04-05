eb57a7ec6de550ac870f108de33556e6cb655fd4
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-04-05 09:01:45 +0200

    Overall use of Interfaces of DTO objects. Added minimum and full Constructors for all DTO objects. Updated IDBController and implemented getIWorkerDTO. Added Cache and ICache Cleaning.

---
+++ src/main/java/cache/Cache.java
@@ -0,0 +1,40 @@
+package cache;
+
+/**
+ * @author Rasmus Sander Larsen
+ */
+public class Cache implements ICache {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+    
+    
+    
+    /*
+    ----------------------- Constructor -------------------------
+     */
+    
+    
+    
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+    // <editor-folder desc="Properties"
+
+
+    // </editor-folder>
+    
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+    
+    
+    
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
---
+++ src/main/java/cache/ICache.java
@@ -0,0 +1,40 @@
+package cache;
+
+/**
+ * @author Rasmus Sander Larsen
+ */
+public interface ICache {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+    
+    
+    
+    /*
+    ----------------------- Constructor -------------------------
+     */
+    
+    
+    
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+    // <editor-folder desc="Properties"
+
+
+    // </editor-folder>
+    
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+    
+    
+    
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
