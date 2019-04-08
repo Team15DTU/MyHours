5344741b703bbb4c035ab331938b39db74ea55f9
nikolajwassmann <nwassmann@hotmail.com>
2019-04-08 19:55:59 +0200

    Added some js and a picture for the modal

--- NewFront/Front.js
+++ NewFront/Front.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $('#trigger').click(function() {
-        $(this).next('#login').slideToggle();
-    });
-});
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
