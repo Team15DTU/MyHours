e5e4d0f3a231454dc151460871ba6d9995c6aef7
Logstor <a.rottger_rydahl@live.dk>
2019-05-30 14:40:24 +0200

    ConnPoolV1.java  - Added Imports  - implements ConnPool Interface  - Added Interface method bodies, and documentation

--- src/main/java/db/connectionPools/ConnPoolV1.java
+++ src/main/java/db/connectionPools/ConnPoolV1.java
@@ -1,23 +1,50 @@
 package db.connectionPools;
 
+import DAO.DALException;
+import db.IConnPool;
+
+import java.sql.Connection;
+
 /**
  * @author Alfred Röttger Rydahl
  */
 
-public class ConnPoolV1 {
-
+public class ConnPoolV1 implements IConnPool {
+	
     /*------------------------------------------------------------
     | Fields                                                     |
-    -------------------------------------------------------------*/    
+    -------------------------------------------------------------*/
     /*------------------------------------------------------------
     | Constructors                                               |
-    -------------------------------------------------------------*/    
+    -------------------------------------------------------------*/
     /*------------------------------------------------------------
     | Properties                                                 |
-    -------------------------------------------------------------*/    
+    -------------------------------------------------------------*/
     /*------------------------------------------------------------
     | Public Methods                                             |
-    -------------------------------------------------------------*/   
+    -------------------------------------------------------------*/
+	
+	/**
+	 * Returns the Connection to the connection pool.
+	 * @param connection The Connection to return
+	 * @throws DALException Data Access Layer Exception
+	 */
+	@Override
+	public void releaseConnection(Connection connection) throws DALException {
+	
+	}
+	
+	/**
+	 * Gives a live Connection to the Database, which needs to be returned after
+	 * use.
+	 * @return Return a Connection object
+	 * @throws DALException Data Access Layer Exception
+	 */
+	@Override
+	public Connection getConn() throws DALException {
+		return null;
+	}
+	
     /*------------------------------------------------------------
     | Private Methods                                            |
     -------------------------------------------------------------*/
