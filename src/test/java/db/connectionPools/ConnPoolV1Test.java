6b65968b8d4a7d0bc98563921e9540230d65adc7
Logstor <a.rottger_rydahl@live.dk>
2019-05-31 16:38:53 +0200

    ConnPoolV1Test.java  - Added imports  - Implemented setUp() and tearDown()  - Implemented getSize()  - Implemented getFreeConns()

--- src/test/java/db/connectionPools/ConnPoolV1Test.java
+++ src/test/java/db/connectionPools/ConnPoolV1Test.java
@@ -1,26 +1,101 @@
 package db.connectionPools;
 
+import DAO.DALException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
 
 import static org.junit.Assert.*;
 
 public class ConnPoolV1Test {
 	
-	@Test
-	public void getInstance()
+	private ConnPoolV1 connPool;
+	private int connSize;
+	
+	@Before
+	public void setUp() throws Exception
 	{
-		//TODO: Implement this!
+		connPool = ConnPoolV1.getInstance();
+		connSize = connPool.getSize();
+	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		try
+		{
+			connPool.finalize();
+		}
+		catch (Throwable e)
+		{
+			System.err.println("ERROR: Couldn't call finalize() in tearDown() - " + e.getMessage());
+		}
 	}
 	
 	@Test
-	public void releaseConnection()
+	public void getSize()
 	{
-		//TODO: Implement this!
+		assertEquals(connSize, connPool.getSize());
 	}
 	
 	@Test
-	public void getConn()
+	public void getFreeConns()
 	{
-		//TODO: Implement this!
+		assertEquals(connSize, connPool.getFreeConns());
+		
+		// Take one connection
+		Connection c = null;
+		try
+		{
+			c = connPool.getConn();
+		}
+		catch (DALException e)
+		{
+			System.err.println("ERROR: Couldn't get a connection from the pool - " + e.getMessage());
+		}
+		
+		assertEquals(connSize-1, connPool.getFreeConns());
+		
+		// Put back connection
+		try
+		{
+			connPool.releaseConnection(c);
+		}
+		catch (DALException e)
+		{
+			System.err.println("ERROR: Couldn't release connection back to the pool - " + e.getMessage());
+		}
+		
+		assertEquals(connSize, connPool.getFreeConns());
+	}
+	
+	@Test
+	public void getUsedConns() {
+	}
+	
+	@Test
+	public void getRefreshRate() {
+	}
+	
+	@Test
+	public void setRefreshRate() {
+	}
+	
+	@Test
+	public void getValidTimeout() {
+	}
+	
+	@Test
+	public void setValidTimeout() {
+	}
+	
+	@Test
+	public void releaseConnection() {
+	}
+	
+	@Test
+	public void getConn() {
 	}
 }
