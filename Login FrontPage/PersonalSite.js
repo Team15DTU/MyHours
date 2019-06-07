2972e7371145dd0a1380674b082b23be81814a30
Anders <s185100@student.dtu.dk>
2019-06-07 17:27:17 +0200

    Fixet personlig tab der ikke lukker når man klikker på en anden personlig tab.

---
+++ Login FrontPage/PersonalSite.js
@@ -0,0 +1,28 @@
+
+// Show only one item at a time in the menu
+var checkMenu0 = function() {
+    //$("#myid").attr("checked");
+    $("#myid1").prop("checked", false);
+    $("#myid2").prop("checked", false);
+    $("#myid3").prop("checked", false);
+};
+
+var checkMenu1 = function() {
+    //$("#myid").attr("checked");
+    $("#myid").prop("checked", false);
+    $("#myid2").prop("checked", false);
+    $("#myid3").prop("checked", false);
+};
+var checkMenu2 = function() {
+    //$("#myid").attr("checked");
+    $("#myid1").prop("checked", false);
+    $("#myid").prop("checked", false);
+    $("#myid3").prop("checked", false);
+};
+
+var checkMenu3 = function() {
+    //$("#myid").attr("checked");
+    $("#myid1").prop("checked", false);
+    $("#myid2").prop("checked", false);
+    $("#myid").prop("checked", false);
+};
