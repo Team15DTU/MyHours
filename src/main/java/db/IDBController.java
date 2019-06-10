01433add7c53a647a18281ab0bda9db12a17432e
Logstor <a.rottger_rydahl@live.dk>
2019-06-10 17:29:32 +0200

    IDBController.java  - Reformatted whole file for readability  - Implemented some getList prototypes

--- src/main/java/db/IDBController.java
+++ src/main/java/db/IDBController.java
@@ -6,32 +6,87 @@ import DTOs.activity.IActivityDTO;
 import DTOs.workPlace.IEmployerDTO;
 import DTOs.worker.IWorkerDTO;
 
+import java.util.List;
+
 /**
  * @author Rasmus Sander Larsen
  */
 public interface IDBController
 {
+    //region Worker
+    
     /*------------------------------------------------------------
     | Creators                                                   |
     -------------------------------------------------------------*/
     void createWorker   (IWorkerDTO workerDTO, String password) throws DALException;
-    void createEmployer (IEmployerDTO employer)                	throws DALException;
-    void createJob      (IJobDTO job)                           throws DALException;
-    void createActivity (IActivityDTO activity)                 throws DALException;
     
     /*------------------------------------------------------------
     | Get singles                                                |
     -------------------------------------------------------------*/
     IWorkerDTO      getIWorkerDTO       (String email)          throws DALException;
     IWorkerDTO      getIWorkerDTO       (int id)                throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get lists                                                  |
+    -------------------------------------------------------------*/
+    List<IWorkerDTO> getIWorkerDTOList  ()                      throws DALException;
+    List<IWorkerDTO> getIWorkerDTOList  (int minID, int maxID)  throws DALException;
+    
+    //endregion
+    
+    //region Employer
+    
+    /*------------------------------------------------------------
+    | Creators                                                   |
+    -------------------------------------------------------------*/
+    void createEmployer (IEmployerDTO employer)                	throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get singles                                                |
+    -------------------------------------------------------------*/
     IEmployerDTO 	getIEmployerDTO     (int id)                throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get lists                                                  |
+    -------------------------------------------------------------*/
+    
+    //endregion
+    
+    //region Job
+    
+    /*------------------------------------------------------------
+    | Creators                                                   |
+    -------------------------------------------------------------*/
+    void createJob      (IJobDTO job)                           throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get singles                                                |
+    -------------------------------------------------------------*/
     IJobDTO         getIJobDTO          (int id)                throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get lists                                                  |
+    -------------------------------------------------------------*/
+    
+    //endregion
+    
+    //region Activity
+    
+    /*------------------------------------------------------------
+    | Creators                                                   |
+    -------------------------------------------------------------*/
+    void createActivity (IActivityDTO activity)                 throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get singles                                                |
+    -------------------------------------------------------------*/
     IActivityDTO 	getIActivity        (int id)                throws DALException;
     
     /*------------------------------------------------------------
     | Get lists                                                  |
     -------------------------------------------------------------*/
     
+    //endregion
     
     /*------------------------------------------------------------
     | Utilities                                                  |
