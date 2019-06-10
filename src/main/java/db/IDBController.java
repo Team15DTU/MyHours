8773bc6fb38d5b236ea26b312e4941ddad7f72fb
Logstor <a.rottger_rydahl@live.dk>
2019-06-10 16:54:36 +0200

    IDBController.java  - Updated interface with simple creates and gets

--- src/main/java/db/IDBController.java
+++ src/main/java/db/IDBController.java
@@ -1,16 +1,40 @@
 package db;
 
 import DAO.DALException;
+import DTOs.job.IJobDTO;
+import DTOs.shift.IShiftDTO;
+import DTOs.workPlace.IWorkPlaceDTO;
 import DTOs.worker.IWorkerDTO;
 
 /**
  * @author Rasmus Sander Larsen
  */
-public interface IDBController  {
-
-    void createWorker(IWorkerDTO workerDTO, String password) throws DALException;
-
+public interface IDBController
+{
+    /*------------------------------------------------------------
+    | Creators                                                   |
+    -------------------------------------------------------------*/
+    void createWorker   (IWorkerDTO workerDTO, String password) throws DALException;
+    void createEmployer (IWorkPlaceDTO employer)                throws DALException;
+    void createJob      (IJobDTO job)                           throws DALException;
+    void createActivity (IShiftDTO activity)                    throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get singles                                                |
+    -------------------------------------------------------------*/
+    IWorkerDTO      getIWorkerDTO       (String email)          throws DALException;
+    IWorkerDTO      getIWorkerDTO       (int id)                throws DALException;
+    IWorkPlaceDTO   getIEmployerDTO     (int id)                throws DALException;
+    IJobDTO         getIJobDTO          (int id)                throws DALException;
+    IShiftDTO       getIActivity        (int id)                throws DALException;
+    
+    /*------------------------------------------------------------
+    | Get lists                                                  |
+    -------------------------------------------------------------*/
+    
+    
+    /*------------------------------------------------------------
+    | Utilities                                                  |
+    -------------------------------------------------------------*/
     String setTimeZoneFromSQLServer() throws DALException;
-
-    IWorkerDTO getIWorkerDTO (String email) throws DALException;
 }
