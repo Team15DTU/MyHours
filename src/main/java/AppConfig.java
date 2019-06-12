60946fb9ba71a2d0a155bb54fe4877d09d2f887c
Logstor <a.rottger_rydahl@live.dk>
2019-06-12 21:58:16 +0200

    AppConfig.java  - Created

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,10 @@
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author Alfred Röttger Rydahl
+ */
+@ApplicationPath("/MyHours")
+public class AppConfig extends ResourceConfig
+{ }
