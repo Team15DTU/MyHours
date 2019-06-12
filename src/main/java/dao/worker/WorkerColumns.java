46b7563c27b3140884c7e12b87e6deb14ab5395c
Logstor <a.rottger_rydahl@live.dk>
2019-06-12 16:14:12 +0200

    WorkerColumns.java  - Created  - Implemented  - Documented

---
+++ src/main/java/dao/worker/WorkerColumns.java
@@ -0,0 +1,16 @@
+package dao.worker;
+
+/**
+ * This enum contains all column names in order. The first name
+ * is the name of the table itself.
+ */
+public enum WorkerColumns
+{
+	Workers,
+	id,
+	firstname,
+	surname,
+	email,
+	birthday,
+	pass
+}
