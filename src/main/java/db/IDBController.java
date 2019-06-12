ad71307e69f1275b949eaa49ae3ad06dbe3638aa
Logstor <a.rottger_rydahl@live.dk>
2019-06-12 22:09:03 +0200

    Removed some imports

--- src/main/java/db/IDBController.java
+++ src/main/java/db/IDBController.java
@@ -5,12 +5,7 @@ import dto.job.IJobDTO;
 import dto.activity.IActivityDTO;
 import dto.employer.IEmployerDTO;
 import dto.worker.IWorkerDTO;
-import dto.worker.WorkerDTO;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import java.util.Date;
 import java.util.List;
 
