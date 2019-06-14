9987e97a11f198d1c41d1d7d3234cd87e1f154cd
ak5ina <43174060+ak5ina@users.noreply.github.com>
2019-06-14 13:43:37 +0200

    merged med dev og gjort om til % i stedet for PX

--- src/main/webapp/PersonalSite/PersonalSite.js
+++ src/main/webapp/PersonalSite/PersonalSite.js
@@ -1,3 +1,4 @@
+//Dette metoderne til tabellen
 
 function shifts() {
 
@@ -45,8 +46,8 @@ function job() {
     for (i = 0; i < 1; i++){
         var row2 = table.insertRow(i+1);
         row2.insertCell(0).innerHTML = findCompany(i);
-        row2.insertCell(1).innerHTML = lastPaycheck(i);
-        row2.insertCell(2).innerHTML = estimatePaycheck(i);
+        row2.insertCell(1).innerHTML = lastPaycheck(i)+' Kr.';
+        row2.insertCell(2).innerHTML = estimatePaycheck(i)+' Kr.';
     }
 }
 
@@ -76,31 +77,55 @@ function none() {
 }
 
 
+//her er de metoder til  felterne som skal snakke med serveren
 
 function findCompany(nr) {
     return 'irma'
 }
 
+function findNrOfCompanys() {
+    return 3;
+}
+
+function findCompanyId(nr) {
+    if (nr == 0){return 1;}
+    if (nr == 1){return 2;}
+    else {return 3}
+}
+
 function lastPaycheck(nr) {
-    return '2500 kr.'
+    return 2500;
 }
 
 function estimatePaycheck(nr) {
-    return '1750 kr.'
+    return 1750;
 }
 
 function findShift(nr) {
     return '16-20';
-
 }
 
 function findWork(nr) {
-    return 'irma';
-
+    if (nr == 0) {return 'kvikly'}
+    else if (nr == 1) {return 'fakta'}
+    else if (nr == 2) {return 'irma'}
+    else if (nr == 3) {return '#VektorLife'}
 }
 
 function findPaycheck(nr) {
     return '444 kr.'
+}
+
+
+//her er graferne
+
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function hoursOfWork () {
+
+    return Math.random()*10;
 }
 
 // Show only one item at a time in the menu
