0444188358f523f9422ddf52e91811db7b3a0f78
niko5297 <43748757+niko5297@users.noreply.github.com>
2019-06-17 11:44:09 +0200

    Merging DBController into RESTPersonalSite (#63)
    * Implementing TestDataController in TestDataController
    
    * Implementing TestDataController in TestDataController
    Finished JobDAOTest
    
    * Implementing TestDataController in TestDataController
    Finished JobDAOTest
    
    * Implementing TestDataController in TestDataController
    Finished JobDAOTest
    
    * Implementing TestDataController in TestDataController
    Set up ActivityDAOTest and made half of tests.
    Made half of ActivityDAO methods.
    
    * Implementing TestDataController in TestDataController
    Set up ActivityDAOTest and made half of tests.
    Made half of ActivityDAO methods.
    
    * Implementing TestDataController in TestDataController
    Set up ActivityDAOTest and made half of tests.
    Made half of ActivityDAO methods.
    
    * ConnPoolV1.java
     - Added docs to getUser()
    
    * ConnPoolV1.java
     - Added some docs to the pool implementation
     - Added TODOs to correct the ConcurrentModificationException
    
    * ConnPoolV1.java
     - closePool() loops has been corrected to not cause ConcurrentModificationException
    
    * ConnPoolV1.java
     - closePool() updated exception handling
    
    * ConnPoolV1.java
     - keepAlive() updated exception prints
     - Correct possible cause of ConcurrentModificationException
    
    * ConnPoolV1.java
     - keepAlive() docs correction
    
    * Deleted folder, because no usage
    
    * ConnPoolV1.java
     - Formatting
    
    * Implementing TestDataController in TestDataController
    Set up ActivityDAOTest and made half of tests.
    Made half of ActivityDAO methods.
    
    * Implementing TestDataController in TestDataController
    Set up ActivityDAOTest and made half of tests.
    Made half of ActivityDAO methods.
    
    * ActivityDAO done.
    ActivityDAOTest done.
    
    * WorkerDAO.java
     - Updated WORKERS_TABLENAME to get from the WorkerConstants.java
     - Created and implemeneted getWorker(int id) with documentation
    
    IWorkerDAO.java
     - Created new getWorker(int id) and documentation
    
    ConnectionHelper.java
     - catchSQLExceptionAndDoRollback() added Cause to DALException arguments
    
    * WorkerDAOTest.java
     - Updated tests for getWorker() so that it now tests the new get on id method
    
    * WorkerDAOTest.java
     - Corrected test so it passes
    
    * WorkerDAOTest.java
     - Updated tearDown() afterClass to erase all Workers in the DB
    
    * WorkerHiberDAO.java
     - Implemented getWorker(int id) with documentation
    
    * WorkerHiberDAOTest.java
     - Added important TODO
    
    * WorkerDAO.java
     - Erased Column enum
    
    * DBControllerTest.java
     - setUp() done
    
    * DBControllerTest.java
     - Small changes

--- src/test/java/db/DBControllerTest.java
+++ src/test/java/db/DBControllerTest.java
@@ -1,14 +1,70 @@
 package db;
 
+import dao.ConnectionHelper;
+import dao.activity.ActivityDAO;
+import dao.activity.IActivityDAO;
+import dao.employer.EmployerDAO;
+import dao.employer.IEmployerDAO;
+import dao.job.IJobDAO;
+import dao.job.JobDAO;
+import dao.worker.IWorkerDAO;
+import dao.worker.WorkerDAO;
+import dto.activity.IActivityDTO;
+import dto.employer.IEmployerDTO;
+import dto.job.IJobDTO;
 import dto.worker.IWorkerDTO;
-import dto.worker.WorkerDTO;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import testData.TestDataController;
 
-import java.time.LocalDate;
+import java.sql.Connection;
 
-import static org.junit.Assert.*;
-
-public class DBControllerTest {
+public class DBControllerTest
+{
+	
+	private IConnPool connPool;
+	
+	@BeforeClass
+	public void setUp() throws Exception
+	{
+		connPool 		 = TestConnPoolV1.getInstance();
+		ConnectionHelper help 	 = new ConnectionHelper(connPool);
+		IWorkerDAO workerDAO 	 = new WorkerDAO(connPool);
+		IEmployerDAO employerDAO = new EmployerDAO(connPool, help);
+		IJobDAO jobDAO 			 = new JobDAO(connPool, help);
+		IActivityDAO activityDAO = new ActivityDAO(connPool, help);
+		
+		IWorkerDTO worker1 = TestDataController.getTestWorkerNo1();
+		IWorkerDTO worker2 = TestDataController.getTestWorkerNo2();
+		IEmployerDTO employer1 = TestDataController.getEmployerNo1(worker1);
+		IEmployerDTO employer2 = TestDataController.getEmployerNo2(worker2);
+		IJobDTO job1 = TestDataController.getJobNo1(employer1);
+		IJobDTO job2 = TestDataController.getJobNo2(employer2);
+		IActivityDTO activity1 = TestDataController.getActivityNo1(job1);
+		IActivityDTO activity2 = TestDataController.getActivityNo2(job2);
+		
+		workerDAO.createWorker(worker1);
+		workerDAO.createWorker(worker2);
+		
+		employerDAO.createiEmployer(employer1);
+		employerDAO.createiEmployer(employer2);
+		
+		jobDAO.createIJob(job1);
+		jobDAO.createIJob(job2);
+		
+		activityDAO.createiActivity(activity1);
+		activityDAO.createiActivity(activity2);
+	}
+	
+	@AfterClass
+	public void tearDown() throws Exception
+	{
+		Connection conn = connPool.getConn();
+		
+		// Delete everything in all tables
+		
+	}
 	
 	@Test
 	public void getiWorkerDAO()
