aa6aa11d73883b164d8b13e0495194f7c4c154d8
Logstor <a.rottger_rydahl@live.dk>
2019-06-18 17:35:48 +0200

    ArrayDBControllerTest.java  - createWorker() tested

--- src/test/java/db/ArrayDBControllerTest.java
+++ src/test/java/db/ArrayDBControllerTest.java
@@ -1,24 +1,49 @@
 package db;
 
 import org.junit.*;
+import org.junit.runners.MethodSorters;
+import testData.TestDataController;
 
 import java.awt.image.AreaAveragingScaleFilter;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.JVM)		// Make sure tests run in order
 public class ArrayDBControllerTest
 {
-	private ArrayDBController controller;
+	private static ArrayDBController controller;
 	
 	@BeforeClass
-	public void setUp() throws Exception
+	public static void setUp() throws Exception
 	{
 		controller = new ArrayDBController();
 	}
 	
 	@AfterClass
-	public void tearDown() throws Exception
+	public static void tearDown() throws Exception
 	{
+	}
+	
+	@Test
+	public void createWorker() throws Exception
+	{
+		// Create two workers
+		controller.createWorker(TestDataController.getTestWorkerNo1());
+		controller.createWorker(TestDataController.getTestWorkerNo2());
+		
+		// Check that they're created
+		assertEquals(2, controller.getWorkerList().size());
+		
+		// Check first worker
+		assertEquals(TestDataController.getTestWorkerNo1().getFirstName(), controller.getWorkerList().get(0).getFirstName());
+		assertEquals(TestDataController.getTestWorkerNo1().getSurName(), controller.getWorkerList().get(0).getSurName());
+		assertEquals(TestDataController.getTestWorkerNo1().getEmail(), controller.getWorkerList().get(0).getEmail());
+		assertEquals(TestDataController.getTestWorkerNo1().getPassword(), controller.getWorkerList().get(0).getPassword());
+		// Check second worker
+		assertEquals(TestDataController.getTestWorkerNo2().getFirstName(), controller.getWorkerList().get(1).getFirstName());
+		assertEquals(TestDataController.getTestWorkerNo2().getSurName(), controller.getWorkerList().get(1).getSurName());
+		assertEquals(TestDataController.getTestWorkerNo2().getEmail(), controller.getWorkerList().get(1).getEmail());
+		assertEquals(TestDataController.getTestWorkerNo2().getPassword(), controller.getWorkerList().get(1).getPassword());
 	}
 	
 	@Test
@@ -28,11 +53,6 @@ public class ArrayDBControllerTest
 	
 	@Test
 	public void setWorkerList()
-	{
-	}
-	
-	@Test
-	public void createWorker()
 	{
 	}
 	
