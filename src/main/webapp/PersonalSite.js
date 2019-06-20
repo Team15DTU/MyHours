f7d5280e6eacbbd90ac9603cd8bdd05b50cde783
niko5297 <s185082@student.dtu.dk>
2019-06-20 23:24:14 +0200

    Revert "All data is now getting displayed"
    This reverts commit 20de3cfc

--- src/main/webapp/PersonalSite.js
+++ src/main/webapp/PersonalSite.js
@@ -553,12 +553,19 @@ function updateGraf(choice) {
                             for (i = 0; i < 3; i++) {
                                 var row2 = table.insertRow(i + 1);
 
-                                row2.insertCell(0).innerHTML = resulte[i]['startingDateTime'].toLocaleString();
-                                row2.insertCell(1).innerHTML = resulte[i]['endingDateTime'].toLocaleString();
+                                var shiftStart = findShift(i)[2];
+                                var shiftEnd = findShift(i)[3];
+
+
+                                var shiftStartString = shiftStart.getDate() + '/' + (shiftStart.getMonth() + 1) + ' ' + with_leading_zeros(shiftStart.getHours()) + ':' + with_leading_zeros(shiftStart.getMinutes()) + ' - ' + with_leading_zeros(shiftEnd.getHours()) + ':' + with_leading_zeros(shiftEnd.getMinutes());
+
+
+                                row2.insertCell(0).innerHTML = resulte[i]['startingDateTime'];
+                                row2.insertCell(1).innerHTML = resulte[i]['endingDateTime'];
                                 row2.insertCell(2).innerHTML = resulte[i]['activityValue'];
                             }
                         }
-                    });
+                    })
                     break;
 
                 case "jobInfo":
@@ -566,8 +573,7 @@ function updateGraf(choice) {
                         method: "GET",
                         url: "/MyHours/ArrayDBController/getEmployerList",
                         dataType: "JSON",
-                        success: function (response) {
-                                $.each(response, function(k) {
+                        success: function (resulte) {
                             var table = document.getElementById('left_table');
                             while (table.hasChildNodes()) {
                                 table.removeChild(table.firstChild);
@@ -587,25 +593,25 @@ function updateGraf(choice) {
                                 //new Date();
                                 //'Insert activity name';
                                 var row2 = table.insertRow(k + 1);
-                                row2.insertCell(0).innerHTML = response[k]['name'];//findJob(i)[2];
+                                row2.insertCell(0).innerHTML = resulte[k]['name'];//findJob(i)[2];
                                 row2.insertCell(1).innerHTML = result[k]['jobName']; //lastPaycheck(findJob(k)[0])+' Kr.';
                                 row2.insertCell(2).innerHTML = result[k]['stdSalary'];
                             });
-                        })
                         }
 
-                    });
+                    })
                     break;
 
 
 
 
                 case "employerInfo":
+
                     $.ajax({
                         method: "GET",
                         url: "/MyHours/ArrayDBController/getEmployerList",
                         dataType: "JSON",
-                        success: function (resulte) {
+                        success: function (result) {
                             var table = document.getElementById('left_table');
                             while (table.hasChildNodes()) {
                                 table.removeChild(table.firstChild);
@@ -617,16 +623,12 @@ function updateGraf(choice) {
                             row.insertCell(0).innerHTML = firm.bold();
                             var i;
 
-                            //TODO INSERT REAL ACTIVITY INFORMATION
                             for (i = 0; i < 3; i++) {
                                 var row2 = table.insertRow(i + 1);
-
-                                row2.insertCell(0).innerHTML = resulte[i]['name'];
+                                row2.insertCell(0).innerHTML = result[i]['name'];
                             }
                         }
-                    });
-                    break;
-
+                    })
             }
         }
     })
