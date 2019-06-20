4967339fa05caa5bd193c9474833bb50c8a76285
niko5297 <s185082@student.dtu.dk>
2019-06-20 22:55:21 +0200

    Fix af Employers der gentages

--- src/main/webapp/PersonalSite.js
+++ src/main/webapp/PersonalSite.js
@@ -373,9 +373,9 @@ function deleteActivityByID(activityID){
 function deleteEmployer() {
     event.preventDefault();
 
-    var employerID = $("#company_employer_delete".employerID).val();
+    var employerID = $("#company_employer_delete").val();
 
-    $("#company_employer_delete".employerID).val(employerID);
+    $("#company_employer_delete").val(employerID);
     console.log(employerID);
 
     deleteEmployerByID(employerID);
@@ -567,7 +567,7 @@ function updateGraf(choice) {
                         url: "/MyHours/ArrayDBController/getEmployerList",
                         dataType: "JSON",
                         success: function (response) {
-                                $.each(response, function(i) {
+                                $.each(response, function(k) {
                             var table = document.getElementById('left_table');
                             while (table.hasChildNodes()) {
                                 table.removeChild(table.firstChild);
@@ -581,12 +581,13 @@ function updateGraf(choice) {
                             row.insertCell(0).innerHTML = firm.bold();
                             row.insertCell(1).innerHTML = lastpay.bold();
                             row.insertCell(2).innerHTML = recivepay.bold();
+                            var i;
 
                             $.each(result, function (k) {
                                 //new Date();
                                 //'Insert activity name';
                                 var row2 = table.insertRow(k + 1);
-                                row2.insertCell(0).innerHTML = response[i]['name'];//findJob(i)[2];
+                                row2.insertCell(0).innerHTML = response[k]['name'];//findJob(i)[2];
                                 row2.insertCell(1).innerHTML = result[k]['jobName']; //lastPaycheck(findJob(k)[0])+' Kr.';
                                 row2.insertCell(2).innerHTML = result[k]['stdSalary'];
                             });
@@ -600,20 +601,15 @@ function updateGraf(choice) {
 
 
                 case "employerInfo":
-
                     $.ajax({
                         method: "GET",
                         url: "/MyHours/ArrayDBController/getEmployerList",
                         dataType: "JSON",
-                        success: function (response) {
-                            $.each(response, function(k) {
+                        success: function (resulte) {
                             var table = document.getElementById('left_table');
                             while (table.hasChildNodes()) {
                                 table.removeChild(table.firstChild);
                             }
-
-                            var option = '';
-
 
                             var firm = 'Employer';
 
@@ -621,15 +617,16 @@ function updateGraf(choice) {
                             row.insertCell(0).innerHTML = firm.bold();
                             var i;
 
+                            //TODO INSERT REAL ACTIVITY INFORMATION
                             for (i = 0; i < 3; i++) {
-                                option += response[k]['name'];
-
                                 var row2 = table.insertRow(i + 1);
-                                row2.insertCell(0).innerHTML = option;
+
+                                row2.insertCell(0).innerHTML = resulte[i]['name'];
                             }
-                        })
                         }
-                    })
+                    });
+                    break;
+
             }
         }
     })
